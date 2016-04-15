a6f685585d2ccc675951e12c96d3e22aabdf05ec
Don Bencilao <Don Bencilao@egg.ph>
2016-04-15 09:27:34 +0800

    remove chat from stable branch

--- www/js/services.js
+++ www/js/services.js
@@ -71,18 +71,3 @@ app.factory('MenuFunctions',function(){
   }
 
 });
-
-
-
-//chat
-
- app.factory('socket',function(socketFactory){
-  //Create socket and connect to http://chat.socket.io 
-  var myIoSocket = io.connect('http://chat.socket.io');
-
-    mySocket = socketFactory({
-      ioSocket: myIoSocket
-    });
-    
-  return mySocket;
-})
